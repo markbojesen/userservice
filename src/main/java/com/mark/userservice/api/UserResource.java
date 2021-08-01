0ce4209a1a23da5fe6e2fa0ba9d0daecf2a3bff7
markbojesen <bojesenmark@gmail.com>
2021-08-01 20:18:41 +0100

    Added userResource

---
+++ src/main/java/com/mark/userservice/api/UserResource.java
@@ -0,0 +1,25 @@
+package com.mark.userservice.api;
+
+import com.mark.userservice.domain.User;
+import com.mark.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserResource {
+
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return  ResponseEntity.ok().body(userService.getUsers());
+    }
+
+}
